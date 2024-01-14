87ef63de658f7ce404d9c1d084755d3cc050df9e
sahithp <sahithpanch@gmail.com>
2024-01-14 03:27:53 -0800

    final changes

--- screens/Mood.jsx
+++ screens/Mood.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import Navigation from '../components/navigation';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as db from 'expo-secure-store';
@@ -8,17 +8,21 @@ import dayjs from 'dayjs';
 const dbKey = () => {return "journal-" + dayjs().format("YYYYMMDD")};
 export default function MoodScreen({navigation}) {
   const [mood, setMood] = React.useState("");
+  const [apiresponse, setapiresponse] = React.useState("");
   const getDayMood = async () => {
     const dayMood = JSON.parse(await db.getItemAsync(dbKey()));
     if (dayMood) return dayMood.q5;
     else return "No mood record today."
   }
+  fetch(process.env.API_URI+"/chat?="+JSON.stringify(getDayMood())).then((response) => {setapiresponse(response.json())});
   getDayMood().then((dayMood) => {setMood(dayMood)});
   return (
       <View className="w-screen h-screen items-center justify-between bg-stone-900">
-        <View className='mt-[90px] w-full items-center'>
+        <ScrollView className='mt-[90px] w-full'>
+          <View className='items-center'>
           <MaterialCommunityIcons name="google-circles" size={50} color="white" />
           <Text className="text-3xl text-white font-semibold">Mood</Text>
+          </View>
           <View className='w-full pt-5 px-5'>
             <View className='bg-stone-800  w-max rounded-xl p-3 px-4 flex'>
               <Text className='font-bold text-stone-500 text-xs '>TODAY'S MOOD</Text>
@@ -28,10 +32,11 @@ export default function MoodScreen({navigation}) {
           <View className='w-full pt-5 px-5'>
             <View className='bg-stone-800  w-max rounded-xl p-3 px-4 flex'>
               <Text className='font-bold text-stone-500 text-xs '>JOURNAL MOOD ANALYSIS</Text>
-              <Text className='font-bold text-white text-lg '>{mood}</Text>
+              <Text className='font-bold text-white text-lg '>{apiresponse}</Text>
+              
             </View>
           </View>
-        </View>
+        </ScrollView>
         <Navigation active="Mood"></Navigation>
       </View>
   );
